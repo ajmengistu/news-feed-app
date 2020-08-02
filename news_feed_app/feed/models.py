bd997e9652541c317af214f266b77af2354dc22e
ajmengistu <35409325+ajmengistu@users.noreply.github.com>
2020-08-02 15:34:28 -0400

    Added *.pyc files to gitignore. Kept db.sqlite3 db in source control because size of the file is not to large. Made first migration using the Category model. Added different news categories using the localhost:8080/admin. General category was chosen to be the default feed category.

--- news_feed_app/feed/models.py
+++ news_feed_app/feed/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+# A model to represent a news category such as Sports and Science.
+# A user is able to filter their news feed.
+class Category(models.Model):
+    name = models.CharField(max_length=200) 
+    selected = models.BooleanField(default=False)
+    date_updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
--- news_feed_app/feed/views.py
+++ news_feed_app/feed/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+categories = [
+      {
+            'category': 'Sports',
+            'selected': True
+      },
+
+      {
+            'category': 'Entertainment',
+            'selected': True
+      },
+]
 # Create your views here.
+def index(request):
+      return render(request, 'feed/feed.html')
