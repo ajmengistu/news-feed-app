d123ba09eb9645baab6e4c670c354fbd00b49bb5
ajmengistu <35409325+ajmengistu@users.noreply.github.com>
2020-08-03 13:53:27 -0400

    Updated .travis.yml file to include a script attribute to run tests. Also, added 1 Django test.

--- news_feed_app/feed/tests.py
+++ news_feed_app/feed/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import *
+from django.utils.timezone import now
 
 # Create your tests here.
+class NewsFeedTestCase(TestCase):
+    def setUp(self):
+        Category.objects.create(name='sports', selected=True)
+        Category.objects.create(name='science', selected=False)
+
+    def test_categories_were_saved_to_db(self):
+        sports = Category.objects.get(name='sports')
+        science = Category.objects.get(name='science')
+
+        self.assertEqual(sports.selected, True)
+        self.assertEqual(science.selected, False)
